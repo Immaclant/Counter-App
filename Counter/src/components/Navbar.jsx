65e66e0d2572c43cae100bcfe106ffbe37235eee
Immaclant <goodnessewaoluwa@gmail.com>
2026-04-06 13:45:38 +0100

    some changed made
    you can pull this

---
+++ Counter/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <header>
+      <nav>
+        <h1>Counter-App</h1>
+        <ul>
+          <li>Counter</li>
+          <li>History</li>
+          <li>Settings</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Navbar;
