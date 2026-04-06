a3e8c5f17b8ba7aa56084b32b0003d2a75db656e
Jacopo <163671349+Graph2020@users.noreply.github.com>
2026-04-06 15:56:42 +0300

    modefie reset func

--- Counter/src/Counter.jsx
+++ Counter/src/Counter.jsx
@@ -9,11 +9,14 @@ function Counter() {
 
   function Subtract() {
     setCounter((prev) => (prev > 0 ? prev - 1 : 0));
-  }
+  } // everything is good here
 
   function Reset() {
-    setCounter((prev) => (prev = 0));
+    setCounter(0);
   }
+
+  // no need for prev = 0 you can just setCounter(0) it will set state to have 0 value
+  // plus you don't need to know previous value of state in order to make it zero
 
   return (
     <main>
